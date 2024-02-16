bd7bf9456a5daf1e9d75b959265e05c5ff2824b6
ASWINKUMAR C M <aswin.trikkur@gmail.com>
2024-02-16 13:21:13 +0530

    first commit

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const NavBar = () => {
+  return (
+    <div>
+        <div> <img src="images/currease logo 1.png" alt="logo" /> </div>
+        <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li><button>Shop</button></li>
+        </ul>
+    </div>
+  )
+}
